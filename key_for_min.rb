bca2e334f7dddf84faf153a3ed3818c66f8652a0
Amanda Contreras <amandaben21@aol.com>
2020-02-03 02:58:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,4 +5,11 @@ def key_for_min_value(name_hash)
 min_value = nil
 min_key = nil
 
-name_hash.each do |key,
+name_hash.each do |key,value|
+  if min_value == nil || value < min_value
+    min_value = value
+    min_key = key
+  end
+end
+
+end
